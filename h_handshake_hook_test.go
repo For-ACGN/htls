c8cd4ad4fd03086994f6967c2508b0bdb0f90df2
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-11 15:40:31 +0800

    add TestOnClientHelloMessage, testReadCert and testCheckError.

---
+++ h_handshake_hook_test.go
@@ -0,0 +1,105 @@
+package htls
+
+import (
+	"bytes"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestOnClientHelloMessage(t *testing.T) {
+	cert, err := LoadX509KeyPair("testdata/rsa_cert.pem", "testdata/rsa_key.pem")
+	testCheckError(t, err)
+
+	chHook := func(hello *ClientHelloMessage) error {
+		secret := make([]byte, 32)
+		secret[0] = 0xFF
+		if bytes.Equal(hello.Random, secret) {
+			hello.ALPNProto = []string{"http/1.1"}
+		}
+		return nil
+	}
+	serverCfg := &Config{
+		Certificates:         []Certificate{cert},
+		NextProtos:           []string{"h2", "http/1.1"},
+		OnClientHelloMessage: chHook,
+	}
+	serverCfg.Random = make([]byte, 32)
+	serverCfg.Random[0] = 0xFE
+	listener, err := Listen("tcp", "127.0.0.1:0", serverCfg.Clone())
+	testCheckError(t, err)
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				return
+			}
+
+			c := conn.(*Conn)
+			err = c.Handshake()
+			testCheckError(t, err)
+
+			err = conn.Close()
+			testCheckError(t, err)
+		}
+	}()
+
+	shHook := func(hello *ServerHelloMessage) error {
+		secret := make([]byte, 32)
+		secret[0] = 0xFE
+		if !bytes.Equal(hello.Random, secret) {
+			return errors.New("invalid server random")
+		}
+		return nil
+	}
+
+	clientCfg := &Config{
+		NextProtos:           []string{"h2", "http/1.1"},
+		RootCAs:              x509.NewCertPool(),
+		OnServerHelloMessage: shHook,
+	}
+	clientCfg.RootCAs.AddCert(testReadCert(t, "rsa_cert.pem"))
+	clientCfg.Random = make([]byte, 32)
+	clientCfg.Random[0] = 0xFF
+
+	conn, err := Dial("tcp", listener.Addr().String(), clientCfg.Clone())
+	testCheckError(t, err)
+
+	err = conn.Handshake()
+	testCheckError(t, err)
+
+	state := conn.ConnectionState()
+	if state.Version != VersionTLS13 {
+		t.Fatal("Version should be 1.3")
+	}
+	if state.NegotiatedProtocol != "http/1.1" {
+		t.Fatal("NegotiatedProtocol should be http/1.1")
+	}
+
+	err = conn.Close()
+	testCheckError(t, err)
+
+	err = listener.Close()
+	testCheckError(t, err)
+}
+
+func testReadCert(t *testing.T, name string) *x509.Certificate {
+	data, err := os.ReadFile(filepath.Join("testdata", name))
+	testCheckError(t, err)
+	block, _ := pem.Decode(data)
+	if block == nil {
+		t.Fatal("invalid PEM block")
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	testCheckError(t, err)
+	return cert
+}
+
+func testCheckError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
