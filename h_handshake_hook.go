c8d5c094675f37ce2db3821b125a974264de0183
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-11 15:39:50 +0800

    add onClientHelloMessage and onServerHelloMessage, add structures.

---
+++ h_handshake_hook.go
@@ -0,0 +1,59 @@
+package htls
+
+import (
+	"slices"
+)
+
+type ClientHelloMessage struct {
+	Random    []byte
+	SessionID []byte
+	ALPNProto []string
+}
+
+type ServerHelloMessage struct {
+	Random    []byte
+	SessionID []byte
+	ALPNProto string
+}
+
+func (c *Conn) onClientHelloMessage(hello *clientHelloMsg) error {
+	if c.config.OnClientHelloMessage == nil {
+		return nil
+	}
+	// clone client hello message
+	msg := &ClientHelloMessage{
+		Random:    slices.Clone(hello.random),
+		SessionID: slices.Clone(hello.sessionId),
+		ALPNProto: slices.Clone(hello.alpnProtocols),
+	}
+	err := c.config.OnClientHelloMessage(msg)
+	if err != nil {
+		return err
+	}
+	// adjust the original hello message
+	hello.random = msg.Random
+	hello.sessionId = msg.SessionID
+	hello.alpnProtocols = msg.ALPNProto
+	return nil
+}
+
+func (c *Conn) onServerHelloMessage(hello *serverHelloMsg) error {
+	if c.config.OnServerHelloMessage == nil {
+		return nil
+	}
+	// clone server hello message
+	msg := &ServerHelloMessage{
+		Random:    slices.Clone(hello.random),
+		SessionID: slices.Clone(hello.sessionId),
+		ALPNProto: hello.alpnProtocol,
+	}
+	err := c.config.OnServerHelloMessage(msg)
+	if err != nil {
+		return err
+	}
+	// adjust the original hello message
+	hello.random = msg.Random
+	hello.sessionId = msg.SessionID
+	hello.alpnProtocol = msg.ALPNProto
+	return nil
+}
