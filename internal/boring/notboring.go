2a02b30b3b692c55403efeec8190fd546b21f264
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-11 13:36:56 +0800

    boring/notboring.go: remove the useless funtions about NewGCM.

--- internal/boring/notboring.go
+++ internal/boring/notboring.go
@@ -1,18 +1,5 @@
 package boring
 
-import (
-	"crypto/cipher"
-	"errors"
-)
-
 const Enabled bool = false
-
-func NewGCMTLS(_ cipher.Block) (cipher.AEAD, error) {
-	return nil, errors.New("boring not implemented")
-}
-
-func NewGCMTLS13(_ cipher.Block) (cipher.AEAD, error) {
-	return nil, errors.New("boring not implemented")
-}
 
 func Unreachable() {}
